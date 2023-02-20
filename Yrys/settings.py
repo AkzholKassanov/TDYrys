46a6d6d520660796622f03886c14216a4819d267
AkzholKassanov <51950143+AkzholKassanov@users.noreply.github.com>
2023-02-20 16:28:03 +0500

    Update settings.py

--- Yrys/settings.py
+++ Yrys/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-med9ie$ky+ph%pq0$n)k^q+*(h)100(olld%ivk-2t-8lb%$re'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['tdyrys-production.up.railway.app']
 
